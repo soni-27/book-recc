1c32df07bb5e35dd8d8f6fe2951b410f084f027e
Aastha <aastha88soni@gmail.com>
2025-01-27 13:22:12 +0530

    add associations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,6 @@
 class Book < ApplicationRecord
-  belongs_to :shelf
+  belongs_to :shelf, optional: true
   belongs_to :user
+  has_one_attached :image
+  validates :name, :author, :about, presence: true
 end
--- app/models/shelf.rb
+++ app/models/shelf.rb
@@ -1,3 +1,5 @@
 class Shelf < ApplicationRecord
   belongs_to :user
+  has_many :books, dependent: :destroy
+  validates :title, presence: true
 end
