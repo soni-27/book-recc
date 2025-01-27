df5158da75dc34bb2aa347afcea6e073e6a9747b
Aastha <aastha88soni@gmail.com>
2025-01-27 20:46:48 +0530

    update views

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,8 @@
 Rails.application.routes.draw do
-  get "books/index"
-  get "books/show"
-  get "books/new"
-  get "books/edit"
-  get "shelves/index"
-  get "shelves/show"
-  get "shelves/new"
-  get "shelves/edit"
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/*
-  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  root "shelves#index"
+  resources :shelves do
+    resources :books, only: [ :index, :show, :edit, :update, :destroy ]
+  end
+  resources :books, only: [ :index, :show, :edit, :update, :destroy ]
 end
